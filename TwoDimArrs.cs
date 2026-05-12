c626da3b60e42cee3cdf9ec3d91a2d6aed4e65b7
Just1ceB <chpshonok@gmail.com>
2026-05-12 15:08:54 +0300

    TwoDimArrays now doesn't crash

--- TwoDimArrs.cs
+++ TwoDimArrs.cs
@@ -1,31 +1,34 @@
 using System.Runtime.ExceptionServices;
 using System.Runtime.Intrinsics.X86;
 using Prjcts;
+
 namespace Prjcts
 {
-    internal Class TwoDimArrs
+    internal class TwoDimArrs
     {
         public static void UnitTest()
         {
-            int[,] grd = new int[19, 4];
-            Matrix.RandomGrades(grd);
-            Console.WriteLine("-------------------------------");
-            Matrix.Print(grd);
-            Console.WriteLine("-------------------------------");
-            Console.WriteLine(Matrix.Avrg(grd));
-            Console.WriteLine("-------------------------------");
-            Console.WriteLine("Row student #3 avrg is: " + Matrix.RowAvrg(grd,2));
-            Console.WriteLine("-------------------------------");
-            Console.WriteLine("Col Test #2 avrg is: " + Matrix.RowAvrg(grd, 1));
+            // int[,] grd = new int[19, 4];
+            // Matrix.RandomGrades(grd);
+            // Console.WriteLine("-------------------------------");
+            // Matrix.Print(grd);
+            // Console.WriteLine("-------------------------------");
+            // Console.WriteLine(Matrix.Avrg(grd));
+            // Console.WriteLine("-------------------------------");
+            // Console.WriteLine("Row student #3 avrg is: " + Matrix.RowAvrg(grd,2));
+            // Console.WriteLine("-------------------------------");
+            // Console.WriteLine("Col Test #2 avrg is: " + Matrix.RowAvrg(grd, 1));
+            Console.WriteLine("SMH");
         }
 
         public static void RandomGrades(int[,] studentsGrades)
         {
-            for (int row = 0; row < grades.GetLength(0); row++)
+            Random rnd = new Random();
+            for (int row = 0; row < studentsGrades.GetLength(0); row++)
             {
-                for (int col = 0; col < grades.GetLength(1); col++)
+                for (int col = 0; col < studentsGrades.GetLength(1); col++)
                 {
-                    grades[row, col] = rnd.Next(55, 101);
+                    studentsGrades[row, col] = rnd.Next(55, 101);
                 }
             }
         }
@@ -47,7 +50,7 @@ namespace Prjcts
         public static double StudentAvrg(int[,] studentsGrades, int student)
         {
             int sum = 0;
-            for (int col = 0; row < grades.GetLength(1); col++)
+            for (int col = 0; col < studentsGrades.GetLength(1); col++)
             {
                 sum += studentsGrades[student, col];
             }
@@ -58,7 +61,7 @@ namespace Prjcts
         public static double ExamAvrg(int[,] studentsGrades, int exam)
         {
             int sum = 0;
-            for (int row = 0; row < grades.GetLength(0); row++)
+            for (int row = 0; row < studentsGrades.GetLength(0); row++)
             {
                 sum += studentsGrades[row, exam];
             }
