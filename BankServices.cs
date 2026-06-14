5134160487bd8cf580a4b00c7d5a173de760660b
Just1ceB <chpshonok@gmail.com>
2026-06-14 11:43:01 +0300

    Added start for the BankServices class

---
+++ BankServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prjcts
+{
+    internal class BankServices
+    {
+        protected const int DEFAULT_ACKS_AMOUNT = 100;
+        BasicAccount[] accounts = new BasicAccount[BankServices.DEFAULT_ACKS_AMOUNT];
+
+        public static void UnitTest()
+        {
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Prjcts
             {
                 repeatChoosing = false;
 
-                Console.Write("Choose what you want to test:\n1 - Checking Account system\n2 - Saving account system\n3 - Basic Account\n4 - Employee class\n5 - Business Account\n0 - Close\n");
+                Console.Write("Choose what you want to test:\n1 - Checking Account system\n2 - Saving account system\n3 - Basic Account\n4 - Employee class\n5 - Business Account\n6 - Bank Services\n0 - Close\n");
                 Console.Write("Choose: ");
 
                 switch (Console.ReadKey().KeyChar)
@@ -41,6 +41,10 @@ namespace Prjcts
                     case '5':
                         Console.WriteLine();
                         BusinessAccount.UnitTest();
+                        break;
+                    case '6':
+                        Console.WriteLine();
+                        BankServices.UnitTest();
                         break;
                     default:
                         Console.WriteLine();
